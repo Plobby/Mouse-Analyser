1029ed2e808122c267380dfd65267da29793d674
wjryan5 <17671540@students.lincoln.ac.uk>
2019-12-17 22:38:23 +0000

    Update gui.py

--- Codebase/gui.py
+++ Codebase/gui.py
@@ -3,22 +3,34 @@ import iomanager
 counter = 0
 
 def import_video():
-    iomanager.get_videos(True)
+   iomanager.get_videos(True)
+
 
 def close_window(): 
     root.destroy()
 
+def open_settings():
+    Toplevel(settings)
+
+
 root = Tk()
 root.title("Welcome to MouseHUB!")
 root.geometry('1200x720')
+root.configure(background='black')
+
+settings = Tk()
+settings.title("Settings")
+settings.geometry("800x600")
+settings.configure(background='black')
+
 
 window = Label(root, text="MouseHUB")
-window.pack()
+window.pack()   
 
-btn = Button(window, text="Import Video", width=25, command = import_video)
+btn = Button(window, text="Import Video", width=25, bg="orange", command = import_video)
 btn.grid(column=1, row=5)
 
-btn1 = Button(window, text="Settings", width=25)
+btn1 = Button(window, text="Settings", bg="orange", width=25, command= open_settings)
 btn1.grid(column=2, row=5)
 
 btn2 = Button(window, text="Exit", width=50, command = close_window)
