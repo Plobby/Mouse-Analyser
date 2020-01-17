def53866bf2cba353f5d672246f5dfd529f6a460
Stuart <stusnout@gmail.com>
2020-01-17 10:29:21 +0000

    Added save_videos function to save batches of videos as .mpg's in chosen directory. Re-ordered VALID_FILES as all files in databases are .mpg.

--- Codebase/iomanager.py
+++ Codebase/iomanager.py
@@ -3,7 +3,7 @@ from tkinter import filedialog
 import cv2
 
 # Create variable with allowed file types
-VALID_FILES = [("mp4 videos", "*.mp4"), ("avi videos", "*.avi"), ("mpeg videos", "*.mpg")]
+VALID_FILES = [("mpeg videos", "*.mpg"), ("mp4 videos", "*.mp4"), ("avi videos", "*.avi")]
 
 # Function to import videos
 def get_videos(multiple):
@@ -22,6 +22,42 @@ def get_videos(multiple):
         videos = [filedialog.askopenfilename(filetypes=VALID_FILES)]
     # Open the videos and return
     return open_files(videos)
+
+#Function to save a *LIST* of CV2 VideoCapture objects
+def save_videos(videoCaps, outputLocation):
+    if outputLocation == None: #This is for GUI mode, where a directory is not already given
+        #Open folder selection dialog box
+        outputLocation = filedialog.askdirectory()
+
+    for video in videoCaps:
+        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH)) #Get frame width
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)) #Get frame height
+        fps = int(video.get(cv2.CAP_PROP_FPS)) #Get video fps
+        fourcc = int(cv2.VideoWriter_fourcc(*'mpg1')) #Fourcc code for writing .mpg video files
+
+        #Concatenate outputLocation with placeholder output.mpg string
+        fileLocation = outputLocation + "/output.mpg"                   #TODO: Create more meaningful filename
+        #Create VideoWriter object using specs of the video being saved
+        out = cv2.VideoWriter(fileLocation, fourcc, fps, (width, height))
+
+        #Set frame position to the first frame of the video
+        video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        #Iterate while video is open
+        while (video.isOpened()):
+            #Read the next return and get the frame
+            ret, frame = video.read()
+            #Break if no frame is returned
+            if (not ret):
+                break
+            
+            #Write frame 
+            out.write(frame)
+
+        #Release VideoCapture object
+        video.release()
+        #Release VideoWriter object
+        out.release()
+
 
 # Function to attempt to open files with openCV
 def open_files(videos):
@@ -47,6 +83,8 @@ def open_files(videos):
             # Wait for the next key
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+        #Save video
+        save_videos([cap], outputLocation=None)
         # Release the capture variable
         cap.release()
         # Destroy all cv2 windows
