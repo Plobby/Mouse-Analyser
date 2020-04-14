447781c987e45a0e82714d6812b90aa66b61273e
Stuart <stusnout@gmail.com>
2020-04-14 20:53:03 +0100

    CLI now processes files

--- Codebase/cli.py
+++ Codebase/cli.py
@@ -3,6 +3,7 @@ import sys
 import iomanager
 import cli_logger
 from cli_logger import LogLevel
+import videoproc
 
 # Function to open the command-line interface with a setp of inputs and an output directory
 def open_cli(inputs, output):
@@ -20,13 +21,18 @@ def open_cli(inputs, output):
         if (not inp.endswith(".mp4") and not inp.endswith(".avi") and not inp.endswith("mpg")):
             cli_logger.log(LogLevel.ERROR, "File provided is not of a valid type (mp4, avi, mpg): \"" + inp + "\"")
             sys.exit(1)
-    # Check the path exists
+    # Check the output path exists
     if (not os.path.exists(output)):
         cli_logger.log(LogLevel.ERROR, "Output directory does not exist at path: \"" + output + "\"")
         sys.exit(1)
-    # Check the path is a directory
+    # Check the output path is a directory
     if (not os.path.isdir(output)):
         cli_logger.log(LogLevel.ERROR, "Output path provided is not a directory: \"" + output + "\"")
         sys.exit(1)
-    # Process the files
-    iomanager.open_files(inputs)
+    
+    #Process each input video
+    for inputVid in inputs:
+        videoproc.processVideo(inputVid, doSaveVid=True, outputLocation=output)
+        cli_logger.log(LogLevel.INFO, inputVid + " finished processing.")
+    
+    cli_logger.log(LogLevel.INFO, "All files processed.")
